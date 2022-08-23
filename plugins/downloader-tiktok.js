c6959967869e3b86a146fb26134ba4e73c23b91c
Diimz Official <111172024+DiimzOfficial@users.noreply.github.com>
2022-08-23 17:21:46 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,6 +1,6 @@
 const { tiktokdl } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
-	if (!args[0]) throw `Link tiktoknya mana?\n\ncontoh:\n${usedPrefix}${command} https://vm.tiktok.com/ZGJAmhSrp/`
+	if (!args[0]) throw `Link tiktoknya mana?`
     tiktokdl(args[0]).then(r => {
     let video = r.video.no_watermark
     conn.sendFile(m.chat, video, '', `*${wm}*`, m)
